43ded16ee6ba1e6df288b2a2fcb8fc57633c8fc7
saipranayteja <pranayteja.ch@algonox.com>
2023-04-17 23:34:22 +0530

    dfgh

--- tags-for-spot-instance/main.tf
+++ tags-for-spot-instance/main.tf
@@ -1,15 +1,15 @@
 resource "aws_ec2_tag" "spot_instance_tag_public" {
     
     resource_id = var.public-spot-id
-    key         = var.key
-    value       = var.component
+    key         = var.public_key
+    value       = var.public_component
 }
 
 resource "aws_ec2_tag" "spot_instance_tag_private" {
     
     resource_id = var.private-spot-id
-    key         = var.key
-    value       = var.component
+    key         = var.private_key
+    value       = var.private_component
 }
 
 
